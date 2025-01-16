9084b11a000b8ef26766280fb608f9343bf7d55c
Oskar Puzio <osk.vitae@gmail.com>
2025-01-16 13:40:46 +0000

    Models Created

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :owner
+  # Associations: This is visualised in DB maker
+  has_many :favourites
+  has_many :reviews
+
+  validates :brand, :model, :year, presence: true
+  # A sort of security feature, ensuring user inputs follow a specific format.
+  validates :year, presence: true, numericality: { only_integer: true }
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,4 @@
 class Owner < ApplicationRecord
+  has_many :cars
+  validates :nickname, uniqueness:true
 end
